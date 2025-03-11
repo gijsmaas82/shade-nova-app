24dc0927dca1e875b9d31fe51b40bf296f2f2f01
Gijs Maas <47821944+gijsmaas82@users.noreply.github.com>
2025-03-11 11:19:05 +0100

    add laserlockdown

--- src/router/index.js
+++ src/router/index.js
@@ -5,12 +5,14 @@ import EscapeRoom from '@/views/EscapeRoom.vue';
 import FeelIT from '@/views/FeelIT.vue';
 import ArgTeam1 from '@/views/ArgTeam1.vue';
 import AgentFromage from '@/views/AgentFromage.vue';
+import LaserLockdown from '@/views/LaserLockdown.vue';
 
 const routes = [
   { path: '/', component: HomePage },
   { path: '/snowowl', component: SnowOwl },
   { path: '/game1', component: ArgTeam1 },
   { path: '/game2', component: AgentFromage },
+  { path: '/game3', component: LaserLockdown },
   { path: '/game4', component: FeelIT },
   { path: '/game5', component: EscapeRoom },
   
---
+++ src/views/LaserLockdown.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="container">
+      <!-- Navigatiebalk -->
+      <div class="nav">
+        <a href="Speluitleg.html">Speluitleg</a>
+      </div>
+  
+      <!-- Hoofdtitel -->
+      <div class="opening">
+        <h1>Laser L🔒ckdown</h1>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'LaserLockdown',
+    methods: {
+      helloWorld() {
+        console.log("Hallo Wereld");
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  /* Algemene stijlen */
+  .container { 
+    font-family: 'Sixtyfour Convergence', sans-serif;
+    background-color: #0f0b45;
+    text-align: center;
+    min-height: 100vh;
+  }
+  
+  .nav {
+    background-color: #333;
+    padding: 15px;
+  }
+  
+  .nav a {
+    color: white;
+    text-decoration: none;
+    font-size: 1.2em;
+  }
+  
+  .nav button {
+    padding: 10px;
+    background-color: #4CAF50;
+    border: none;
+    color: white;
+    cursor: pointer;
+  }
+  
+  .nav button:hover {
+    background-color: #45a049;
+  }
+  
+  .opening h1 {
+    font-size: 2.5em;
+    margin-top: 20px;
+    color: white;
+  }
+  </style>
+  
