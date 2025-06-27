c988d7af497c891fdba00451fc92f27c9ce7144e
Gijs Maas <47821944+gijsmaas82@users.noreply.github.com>
2025-06-27 14:24:30 +0200

    added pwa functionality

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,35 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+
+  pwa: {
+    name: 'S.H.A.D.E. Initiatie',
+    themeColor: '#000000',
+    backgroundColor: '#000000',
+    display: 'standalone',
+    orientation: 'portrait',
+    start_url: '.',
+    manifestOptions: {
+      short_name: 'SHADE',
+      description: 'Een geheim genootschap test je digitale skills',
+      icons: [
+        {
+          src: './img/icons/web-app-manifest-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: './img/icons/web-app-manifest-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    },
+    // Optioneel: eigen service-worker gedrag aanpassen
+    // workboxOptions: {
+    //   skipWaiting: true,
+    //   clientsClaim: true
+    // }
+  }
+});
